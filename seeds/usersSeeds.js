733d9f22016f91aaab0aa32e0911094ef83d8e34
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-06 00:02:54 -0700

    set up form and mongoose

--- seeds/usersSeeds.js
+++ seeds/usersSeeds.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
         name: String,
         password: String,
-        email: String
+        email: String,
+        file: String,
     });
 
-    const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-    const joe = new User({
-        name: 'Joe',
-        password: '123456',
-        email: 'joe@gmail.com'
-    })
+    // const joe = new User({
+    //     name: 'Joe',
+    //     password: '123456',
+    //     email: 'joe@gmail.com'
+    // })
 
-    await joe.save();
-    console.log(joe);
+    // await joe.save();
+    // console.log(joe);
+
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const User = require('./seeds/usersSeeds');
 const app = express();
 
 const port = 8080;
@@ -8,30 +11,30 @@ const port = 8080;
 
 const main = async() => {
     await mongoose.connect('mongodb://localhost:27017/usersDb');
-
-    const userSchema = new mongoose.Schema({
-        name: String,
-        password: String,
-        email: String
-    });
-
-    const User = mongoose.model('User', userSchema);
-
-    const joe = new User({
-        name: 'Joe',
-        password: '123456',
-        email: 'joe@gmail.com'
-    })
-
-    await joe.save();
-    console.log(joe);
 }
 
 main().catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('home');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search-bar');
+})
+
+app.post('/search', async(req, res) => {
+    const formData = req.body;
+    console.log(formData);
+    const user = new User(formData);
+    await user.save();
+    res.redirect('/');
 })
 
 app.listen(port, () => {
